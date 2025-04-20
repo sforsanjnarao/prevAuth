91843a867267de18e395fe9b7e069c0eaacb5351
sforsanjnarao <sanjnarao26@gmail.com>
2025-04-21 01:01:46 +0530

    Add appTracker route to server for tracking functionality

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRouter from './src/routes/auth.route.js';
 import userRouter from './src/routes/user.router.js';
 import vaultRouter from './src/routes/vault.route.js';
 import fakerDataRouter from './src/routes/fakerData.route.js'; 
+import appTracker from './src/routes/appTracker.js'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +23,7 @@ app.use('/api/auth',authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/vault', vaultRouter);
 app.use('/api/fakedata', fakerDataRouter);
+app.use('/api/tracker',appTracker)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
